966007ebe28bc46de7a4769b13314bde0baa5142
aminulislammidday711-maker <aminulislammidday711@gmail.com>
2026-09-13 09:10:48 +0530

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -1,2 +1,2 @@
-// Change this after deploying the backend.
-window.MONOROM_API_URL = window.MONOROM_API_URL || 'https://monorom32.onrender.com/api';
+// Production backend URL (Render)
+window.MONOROM_API_URL = 'https://monorom32.onrender.com/api';
